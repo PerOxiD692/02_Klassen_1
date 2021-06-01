7b3d0b8382958e623354790bf5e6ec4eb2f73393
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-01 14:49:11 +0200

    function ausgabe ++

--- src/App.java
+++ src/App.java
@@ -3,15 +3,22 @@ public class App {
     public static void main(String[] args) {
         
         Cat cat1 = new Cat("Grizabella", "white", 29);
-        System.out.println("name: " + cat1.tellYourAttributes("#name"));
-        System.out.println("color: " + cat1.tellYourAttributes("#color"));
-        System.out.println("age: " + cat1.tellYourAttributes("#age"));
+        ausgabe("name: " + cat1.tellYourAttributes("#name"));
+        ausgabe("color: " + cat1.tellYourAttributes("#color"));
+        ausgabe("age: " + cat1.tellYourAttributes("#age"));
 
-        System.out.println("-----------------------------------");
+        ausgabe("-----------------------------------");
         
         Cat cat2 = new Cat("Alonzo", "black", 35);
-        System.out.println("name: " + cat2.tellYourAttributes("#name"));
-        System.out.println("color: " + cat2.tellYourAttributes("#color"));
-        System.out.println("age: " + cat2.tellYourAttributes("#age"));
+        ausgabe("name: " + cat2.tellYourAttributes("#name"));
+        ausgabe("color: " + cat2.tellYourAttributes("#color"));
+        ausgabe("age: " + cat2.tellYourAttributes("#age"));
     }
+
+    public static void ausgabe (String outputStr){
+         System.out.println(outputStr);
+    }
+
+
+
 }

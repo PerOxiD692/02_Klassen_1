8243f06a926952365427f37eac70fbfc8b4a4864
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-01 09:22:06 +0200

    Wertzuweisung von außen

--- src/App.java
+++ src/App.java
@@ -2,8 +2,18 @@ public class App {
     
     public static void main(String[] args) {
         
-        Cat cat = new Cat();
-        System.out.println("Blick von aussen: " + cat);
-        cat.tellYourAdress();
+        Cat cat1 = new Cat();
+        System.out.println("Blick von aussen: " + cat1);
+        cat1.tellYourAdress();
+        cat1.name = "Grizabella";
+        cat1.tellYourName();
+
+        System.out.println("-----------------------------------");
+        
+        Cat cat2 = new Cat();
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.tellYourAdress();
+        cat2.name = "Alonzo";
+        cat2.tellYourName();
     }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,14 @@
 public class Cat {
+
+    // Property | Attribut | Field
+    public String name = "nobody";
+
     public void tellYourAdress(){
         System.out.println("Blick von innen: " + this);
     }
+
+    public void tellYourName(){
+        System.out.println("Blick von innen (Name): " + this.name);
+    }
+
 }

6da4587b62312de051c9a44326719df0e8781a05
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-01 11:02:02 +0200

    Add Constructor

--- src/App.java
+++ src/App.java
@@ -2,18 +2,12 @@ public class App {
     
     public static void main(String[] args) {
         
-        Cat cat1 = new Cat();
-        System.out.println("Blick von aussen: " + cat1);
-        cat1.tellYourAdress();
-        cat1.name = "Grizabella";
+        Cat cat1 = new Cat("Grizabella", "white", 29);
         cat1.tellYourName();
 
         System.out.println("-----------------------------------");
         
-        Cat cat2 = new Cat();
-        System.out.println("Blick von aussen: " + cat2);
-        cat2.tellYourAdress();
-        cat2.name = "Alonzo";
+        Cat cat2 = new Cat("Alonzo", "black", 35);
         cat2.tellYourName();
     }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,7 +1,15 @@
 public class Cat {
 
     // Property | Attribut | Field
-    public String name = "nobody";
+    public String name;
+    public String furColor;
+    public int age;
+
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
 
     public void tellYourAdress(){
         System.out.println("Blick von innen: " + this);
@@ -9,6 +17,10 @@ public class Cat {
 
     public void tellYourName(){
         System.out.println("Blick von innen (Name): " + this.name);
+    }
+
+    public void changeYourName(String name) {
+        this.name = name;
     }
 
 }
